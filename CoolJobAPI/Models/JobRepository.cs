4a231bf45f1cc4fdd6e683f841129d31fd8255c4
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-03-17 16:22:30 +0100

    Dynamic search is implemented

--- CoolJobAPI/Models/JobRepository.cs
+++ CoolJobAPI/Models/JobRepository.cs
@@ -61,7 +61,7 @@ namespace CoolJobAPI.Models
             string correctValue = filterValue.Replace("%20", " ");
 
             // Get the job object what have the specific property (variable) with the specific value
-            var filtered = _context.Jobs.ToList().Where(job => job.GetType().GetProperty(filterBy)?.GetValue(job).ToString().ToLower() == correctValue);
+            var filtered = _context.Jobs.ToList().Where(job => (bool)job.GetType().GetProperty(filterBy)?.GetValue(job).ToString().ToLower().Contains(correctValue)); 
             return filtered.Where((job, i) => i < 10 * pageNum);
         }
 
