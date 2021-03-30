159e4df8cd9dfc77c87544040fe6ff128f324752
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-03-30 11:12:07 +0200

    Modified string id to int id

--- CoolJobAPI/Models/User.cs
+++ CoolJobAPI/Models/User.cs
@@ -5,7 +5,7 @@ namespace CoolJobAPI.Models
     public class User
     {
         [Key]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public string PasswordSalt { get; set; }
