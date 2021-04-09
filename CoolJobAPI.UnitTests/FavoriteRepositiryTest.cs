bd98825483b6c1ba2faa8cd56ad090fc6b78fd9b
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-04-09 12:12:36 +0200

    Created unit test project

---
+++ CoolJobAPI.UnitTests/FavoriteRepositiryTest.cs
@@ -0,0 +1,28 @@
+using System;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace CoolJobAPI.UnitTests
+{
+    [TestFixture]
+    public class FavoriteRepositoryTest
+    {
+        /*
+        [SetUp]
+        public void Setup()
+        {
+        }
+        */
+
+        public FavoriteRepositoryTest()
+        {
+
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
