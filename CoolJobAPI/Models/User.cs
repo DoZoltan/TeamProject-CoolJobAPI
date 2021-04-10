71a7892bb885d2af5b18f389b16cf26530ce23ee
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-04-10 12:57:11 +0200

    Added new fields to user model (the fields from the development branch)

--- CoolJobAPI/Models/User.cs
+++ CoolJobAPI/Models/User.cs
@@ -8,6 +8,12 @@ namespace CoolJobAPI.Models
         [Key]
         public int Id { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string ProfilePicture { get; set; }
+        public DataType BirthDate { get; set; }
+        public DataType RegistrationDate { get; set; }
         public string Password { get; set; }
         public string PasswordSalt { get; set; }
         public ICollection<Job> Jobs { get; set; }
