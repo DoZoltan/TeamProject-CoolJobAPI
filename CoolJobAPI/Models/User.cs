eb4b2da10f4e5617119919750a379104e18d4d0c
Roland Horváth <rolandhorvath1025@gmail.com>
2021-04-14 11:59:47 +0200

    Use DateTime for dates

--- CoolJobAPI/Models/User.cs
+++ CoolJobAPI/Models/User.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace CoolJobAPI.Models
 {
@@ -12,8 +13,8 @@ namespace CoolJobAPI.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string ProfilePicture { get; set; }
-        public DataType BirthDate { get; set; }
-        public DataType RegistrationDate { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegistrationDate { get; set; }
         public string Password { get; set; }
         public string PasswordSalt { get; set; }
         public ICollection<Job> Jobs { get; set; }
