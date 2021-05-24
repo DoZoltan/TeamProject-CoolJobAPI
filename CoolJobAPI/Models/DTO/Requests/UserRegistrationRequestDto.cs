83f3ffb74149cf33b47f8b491fb271edab78a07b
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-24 13:15:05 +0200

    New properties

--- CoolJobAPI/Models/DTO/Requests/UserRegistrationRequestDto.cs
+++ CoolJobAPI/Models/DTO/Requests/UserRegistrationRequestDto.cs
@@ -14,5 +14,11 @@ namespace CoolJobAPI.Models.DTO.Requests
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string ProfilePicture { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
     }
 }
