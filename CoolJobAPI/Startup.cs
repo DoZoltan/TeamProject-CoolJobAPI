df118c440f9727b0ae4dd209d02c0b61784c286e
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-24 08:59:30 +0200

    Added JwtConfiguration

--- CoolJobAPI/Startup.cs
+++ CoolJobAPI/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using CoolJobAPI.Models;
 using System.Linq;
 using Microsoft.AspNetCore.Identity;
+using CoolJobAPI.Configuration;
 
 namespace CoolJobAPI
 {
@@ -47,6 +48,7 @@ namespace CoolJobAPI
             }
 
             services.AddIdentityCore<User>().AddEntityFrameworkStores<JobContext>();
+            services.Configure<JwtConfig>(Configuration.GetSection("JwtConfig"));
 
             services.AddControllers();
         }
