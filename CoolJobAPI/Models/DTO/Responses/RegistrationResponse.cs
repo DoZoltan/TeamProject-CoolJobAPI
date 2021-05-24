1213b7cd87a73945e45d942a37ddb5b1ef23bcec
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-24 11:08:00 +0200

    CreatedRegistrationResponse and UserRegistrationRequestDto files

---
+++ CoolJobAPI/Models/DTO/Responses/RegistrationResponse.cs
@@ -0,0 +1,12 @@
+﻿using CoolJobAPI.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolJobAPI.Models.DTO.Responses
+{
+    public class RegistrationResponse : AutResult
+    {
+    }
+}
