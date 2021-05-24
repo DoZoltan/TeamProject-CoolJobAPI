91d23e95d31411c8ce8322c069d1032102f0772d
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-24 07:12:45 +0200

    The IdentityUser is no longer inherited.

--- CoolJobAPI/Models/User.cs
+++ CoolJobAPI/Models/User.cs
@@ -5,19 +5,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CoolJobAPI.Models
 {
-    public class User : IdentityUser
+    public class User 
     {
         [Key]
-        //public int Id { get; set; }  --> in IdentityUser
-        //public string UserName { get; set; }  --> in IdentityUser
+        public int Id { get; set; }  //--> in IdentityUser
+        public string UserName { get; set; }  //--> in IdentityUser
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        //public string Email { get; set; }  --> in IdentityUser
+        public string Email { get; set; }  //--> in IdentityUser
         public string ProfilePicture { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime RegistrationDate { get; set; }
-        //public string Password { get; set; }  --> in IdentityUser
-        //public string PasswordSalt { get; set; }  --> in IdentityUser
+        public string Password { get; set; }  //--> in IdentityUser
+        public string PasswordSalt { get; set; }  //--> in IdentityUser
         public ICollection<Job> Jobs { get; set; }
         public ICollection<Favorite> Favorites { get; set; }
     }
