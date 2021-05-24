1d85fdce7f49ec688fd9d5d742403ff2f4c3b6a8
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-24 13:15:24 +0200

    Deleted comments

--- CoolJobAPI/Models/User.cs
+++ CoolJobAPI/Models/User.cs
@@ -8,16 +8,12 @@ namespace CoolJobAPI.Models
     public class User : IdentityUser
     {
         [Key]
-        public new int Id { get; set; }  //--> in IdentityUser
-        //public string UserName { get; set; }  //--> in IdentityUser
+        public new int Id { get; set; }  // I think we have to use the IdentityUser's ID at controllers and repositories
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        //public string Email { get; set; }  //--> in IdentityUser
         public string ProfilePicture { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime RegistrationDate { get; set; }
-        //public string Password { get; set; }  //--> in IdentityUser
-        //public string PasswordSalt { get; set; }  //--> in IdentityUser
         public ICollection<Job> Jobs { get; set; }
         public ICollection<Favorite> Favorites { get; set; }
     }
