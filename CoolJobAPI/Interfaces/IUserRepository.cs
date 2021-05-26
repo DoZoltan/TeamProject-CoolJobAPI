87b57e47622284b5517b623c423191e3103f0eac
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-26 15:05:00 +0200

    Create IUserRepository.cs

---
+++ CoolJobAPI/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using CoolJobAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolJobAPI.Interfaces
+{
+    interface IUserRepository
+    {
+        Task<User> GetByEmail(string email);
+        Task<User> GetByUserName(string userName);
+    }
+}
