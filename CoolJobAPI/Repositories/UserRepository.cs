bddaa1ede9a57bdf00d7eaa7df0f6b27780dbdc7
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-26 15:05:56 +0200

    Create UserRepository.cs

---
+++ CoolJobAPI/Repositories/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using CoolJobAPI.Interfaces;
+using CoolJobAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolJobAPI.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<User> GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByUserName(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
