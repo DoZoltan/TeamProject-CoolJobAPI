ee6242bd611e6e06a4e77ae93b66fb0337745955
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-27 07:50:27 +0200

    Inject JobContext and UserManager<User>
    And implemented the methods from the interface

--- CoolJobAPI/Repositories/UserRepository.cs
+++ CoolJobAPI/Repositories/UserRepository.cs
@@ -1,5 +1,7 @@
 ﻿using CoolJobAPI.Interfaces;
 using CoolJobAPI.Models;
+using CoolJobAPI.Models.DTO.Requests;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +11,47 @@ namespace CoolJobAPI.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        public Task<User> GetByEmail(string email)
+        private readonly JobContext _context;
+        private readonly UserManager<User> _userManager;
+
+
+        public UserRepository(JobContext context, UserManager<User> userManager)
         {
-            throw new NotImplementedException();
+            _context = context;
+            _userManager = userManager;
         }
 
-        public Task<User> GetByUserName(string userName)
+        public async Task<User> CreateUser(UserRegistrationRequestDto user)
         {
-            throw new NotImplementedException();
+            var newUser = new User()
+            {
+                Email = user.Email,
+                UserName = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                ProfilePicture = user.ProfilePicture,
+                BirthDate = user.BirthDate,
+                RegistrationDate = DateTime.Now
+            };
+
+            var isCreated = await _userManager.CreateAsync(newUser, user.Password);
+            
+            if (isCreated.Succeeded)
+            {
+                return newUser;
+            }
+
+            return null;
+        }
+
+        public async Task<User> GetByEmail(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<User> GetByUserName(string userName)
+        {
+            return await _userManager.FindByNameAsync(userName);
         }
     }
 }
