fe2fe1aa84178e7180118c630bb19dc62939c894
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-29 06:36:31 +0200

    Create SuccessfulAuthResponse.cs

---
+++ CoolJobAPI/Models/DTO/Responses/SuccessfulAuthResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolJobAPI.Models.DTO.Responses
+{
+    public class SuccessfulAuthResponse
+    {
+        public string Token { get; set; }
+    }
+}
