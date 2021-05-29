8847807f1357762db95359cbcca6ff8b7bd35021
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-29 04:54:32 +0200

    Use [EmailAddress] attribute at Email property

--- CoolJobAPI/Models/DTO/Requests/UserLoginRequestDto.cs
+++ CoolJobAPI/Models/DTO/Requests/UserLoginRequestDto.cs
@@ -9,6 +9,7 @@ namespace CoolJobAPI.Models.DTO.Requests
     public class UserLoginRequestDto
     {
         [Required]
+        [EmailAddress]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
