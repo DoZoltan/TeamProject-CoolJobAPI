ef4cecfc97b9e44bb5c377f48bd204a0af7b34c5
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-30 06:38:44 +0200

    Added Create method

--- CoolJobAPI/Interfaces/IRefreshTokenRepository.cs
+++ CoolJobAPI/Interfaces/IRefreshTokenRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CoolJobAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,6 @@ namespace CoolJobAPI.Interfaces
 {
     interface IRefreshTokenRepository
     {
+        Task<string> Create(RefreshToken refreshtoken);
     }
 }
