935bd007751288896a35629ee3c018d53c9cfc1e
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-30 06:33:11 +0200

    Create RefreshTokenRepository.cs

---
+++ CoolJobAPI/Repositories/RefreshTokenRepository.cs
@@ -0,0 +1,12 @@
+﻿using CoolJobAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoolJobAPI.Repositories
+{
+    public class RefreshTokenRepository : IRefreshTokenRepository
+    {
+    }
+}
