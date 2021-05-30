8bae4f68e3dfe6376e37a034e8e4a0558e0dee02
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-30 07:09:47 +0200

    Implemented SaveToken method

--- CoolJobAPI/Repositories/RefreshTokenRepository.cs
+++ CoolJobAPI/Repositories/RefreshTokenRepository.cs
@@ -1,4 +1,6 @@
 ﻿using CoolJobAPI.Interfaces;
+using CoolJobAPI.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +10,27 @@ namespace CoolJobAPI.Repositories
 {
     public class RefreshTokenRepository : IRefreshTokenRepository
     {
+        JobContext _context;
+
+
+        public RefreshTokenRepository(JobContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> SaveToken(RefreshToken refreshToken)
+        {
+            try
+            {
+                await _context.RefreshTokens.AddAsync(refreshToken);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
