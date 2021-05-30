160de7ab48ce04a7d656eba2109a759eb9538d25
DoZoltan <61535048+DoZoltan@users.noreply.github.com>
2021-05-30 06:39:21 +0200

    Removed unnecessary properties

--- CoolJobAPI/Models/RefreshToken.cs
+++ CoolJobAPI/Models/RefreshToken.cs
@@ -12,11 +12,6 @@ namespace CoolJobAPI.Models
         public int Id { get; set; }
         public string UserId { get; set; }
         public string Token { get; set; }
-        public string JwtId { get; set; }
-        public bool IsUsed { get; set; }
-        public bool IsRevoked { get; set; }
-        public DateTime AddedDate { get; set; }
-        public DateTime ExpiryDate { get; set; }
 
         [ForeignKey(nameof(UserId))]
         public User User { get; set; }
